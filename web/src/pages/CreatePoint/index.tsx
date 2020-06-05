388971b9771672801e3e3057431efec930418b38
Diana Coelho <coelhodiana04@gmail.com>
2020-06-04 23:54:21 -0300

    dia2 frontend: buscando itens de coleta da API

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,14 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
+import { inflate } from 'zlib';
+
+//Com typescript sempre que criarmos um estado para array ou objeto é necessário informar manualmente o tipo da variável que será armazenada
+
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
 
 const CreatePoint = () => {
+    const [ items, setItems ] = useState<Item[]>([]);
+
     useEffect(() => {
-        
+        api.get('items').then(response => {
+            setItems(response.data);
+        })
     }, []);
 
     return ( 
@@ -95,35 +108,13 @@ const CreatePoint = () => {
                         <span>Selecione um ou mais itens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
+                        {items.map(item => (
+                            <li key={item.id}>
+                                <img src={item.image_url} alt={item.title} />
+                                <span>{item.title}</span>
+                            </li>
+                        ))}
 
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
-
-                        <li className="selected">
-                            <img src="http://localhost:3333/uploads/oleo.svg" alt="óleo"/>
-                            <span>Óleo de cozinha</span>
-                        </li>
                     </ul>
                 </fieldset>
                 <button type="submit">
