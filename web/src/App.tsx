6bdf08c4fc44c9d8b03d67e01ecc309f12c0a634
Diana Coelho <coelhodiana04@gmail.com>
2020-06-04 22:54:25 -0300

    dia 2 frontend: cadastro ponto de coleta

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+
+import Routes from './routes';
 
 import './App.css';
 
 function App(){
-    const [ counter, setCounter ] = useState(0);
-
-    function handleIncrement() {
-        setCounter(counter + 1);
-    }
-
-    function handleDecrement() {
-        setCounter(counter - 1);
-    }
-
     return ( 
-        <div>
-            <Header title={ `Contador: ${ counter }`}/>
-            <button type='button' onClick={handleIncrement}> + </button>
-            <button type='button' onClick={handleDecrement}> - </button>
-        </div>
+        <Routes />
      );
 }
  
